96f63424840c4127fefab6798dbd63ea149c6a01
Cengizhan Topçu <64609951+cengizhantopcu53@users.noreply.github.com>
2023-05-21 11:28:03 +0300

    Update gps.c

--- Konum Bilgisi/konum_bilgisi.neo6m/gps.c
+++ Konum Bilgisi/konum_bilgisi.neo6m/gps.c
@@ -16,10 +16,8 @@ double convertDegMinToDecDeg (float degMin)
   double min = 0.0;
   double decDeg = 0.0;
  
-  //get the minutes, fmod() requires double
   min = fmod((double)degMin, 100.0);
  
-  //rebuild coordinates in decimal degrees
   degMin = (int) ( degMin / 100 );
   decDeg = degMin + ( min / 60 );
  
